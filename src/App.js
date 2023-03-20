b20db123a33ff0b9eb28f2161b99b189e87441fa
Vishal k <vkulkarni149@gmail.com>
2023-03-20 12:16:27 +0530

    Added the Router dom commands in the App.j and index.js to route the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './component/Home/Home';
 
 function App() {
   return (
-    <div>sad</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <ColorModeScript />
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+
   </StrictMode>
 );
 
