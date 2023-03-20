de23c9debbccb94bbc91fca7f1871ca5149275aa
Vishal k <vkulkarni149@gmail.com>
2023-03-20 15:47:40 +0530

    Created the Home page with chakra UI ,Box,   Button,   Heading,   HStack,   Image,   Link,   Stack,   Text,   VStack, and used reactIcons

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-
+import React from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Image,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import './home.css';
+import homImg from '../../assets/images/bg.png';
+import introvideo from '../../assets/videos/intro.mp4';
+import { CgGoogle, CgYoutube } from 'react-icons/cg';
+import { SiCoursera, SiUdemy } from 'react-icons/si';
+import { DiAws } from 'react-icons/di';
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+  return (
+    <section className="home">
+      <div className="container">
+        <Stack
+          direction={['column', 'row']}
+          height="100%"
+          justifyContent={['center', 'space-between']}
+          alignItems="center"
+          spacing={['16', '56']}
+        >
+          <VStack width={'full'} alignItems={['center', 'flex-end']}>
+            <Heading
+              children="LEARN FROM THE EXPERTS"
+              size={'2xl'}
+              //   alignItems="center"
+            />
+            <Text children="Get the Valuable content here at reasonable price " />
+            <Link as={ReactRouterLink} to="/courses">
+              <Button size={'lg'} colorScheme="yellow">
+                Explore More
+              </Button>
+            </Link>
+          </VStack>
+          <Image
+            className="homImgGraphics"
+            boxSize={'md'}
+            src={homImg}
+            objectFit="contain"
+          />
+        </Stack>
+      </div>
+      <Box bg="blackAlpha.800">
+        <Heading
+          children="OUR BRAND"
+          textAlign={'center'}
+          color={'yellow.400'}
+          fontFamily="body"
+          padding={'auto'}
+        />
+        <HStack className="banner" justifyContent={'space-evenly'}>
+          <CgGoogle />
+          <CgYoutube />
+          <SiCoursera />
+          <SiUdemy />
+          <DiAws />
+        </HStack>
+      </Box>
+      <div className="container2">
+        <video
+          autoPlay
+          controls
+          controlsList="nodownload nofullscreen noremoteplayback"
+          disablepictureInPicture
+          disableRemotePlayback
+          src={introvideo}
+        ></video>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
