ffb59eec80a808ff4aa444015c629deab68f0376
Vishal k <vkulkarni149@gmail.com>
2023-03-21 11:43:51 +0530

    created the type of leatures

--- src/component/Courses/Courses.jsx
+++ src/component/Courses/Courses.jsx
@@ -1,10 +1,35 @@
-import { Container, Heading, Input } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Input,
+  Text,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 
 export const Courses = () => {
   const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
+
+  const categories = [
+    'web-Development',
+    'Artifical intelligence',
+    'App development',
+    'Game Dvelopment',
+    'Automation Testing',
+  ];
   return (
-    <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
+    <Container
+      minH={'95vh'}
+      maxW="container.lg"
+      paddingY={'8'}
+      //   css={{
+      //     '&::-webkit-scrollbar': {
+      //       display: 'none',
+      //     },
+      //   }}
+    >
       <Heading children="All COURSES" margin={'8'} />
       <Input
         value={keyword}
@@ -13,6 +38,13 @@ export const Courses = () => {
         placeholder={'Search the Course...'}
         focusBorderColor="yellow.500"
       />
+      <HStack overflowX={'auto'} paddingY="8">
+        {categories.map(item => (
+          <Button onClick={() => setCategory(item)} minW={'60'}>
+            <Text children={item} />
+          </Button>
+        ))}
+      </HStack>
     </Container>
   );
 };
