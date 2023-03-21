8e70b5338f708d94bee8f3a4cd29658e11d49d76
Vishal k <vkulkarni149@gmail.com>
2023-03-21 12:24:45 +0530

    Created the course catalog using image heading and text from chakra ui

--- src/component/Courses/Courses.jsx
+++ src/component/Courses/Courses.jsx
@@ -3,10 +3,47 @@ import {
   Container,
   Heading,
   HStack,
+  Image,
   Input,
+  Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+
+const Course = ({
+  view,
+  title,
+  imgSrc,
+  id,
+  addToPlaylistHandler,
+  creator,
+  description,
+  lectureCount,
+}) => {
+  return (
+    <VStack className="course" alignItems={['center', 'flex-start']}>
+      <Image src={imgSrc} boxSize="60" objectFit={'contain'} />
+      <Heading
+        children={title}
+        size={'sm'}
+        textAlign={['center', 'flex-start']}
+        maxW="200px"
+        fontFamily={'sans-serif'}
+        noOfLines={3}
+      />
+      <Text children={description} noOfLines={2} />
+      <HStack>
+        <Text
+          children={'creator'}
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+        />
+        <Text children={creator} fontFamily={'body'} />
+      </HStack>
+    </VStack>
+  );
+};
 
 export const Courses = () => {
   const [keyword, setKeyword] = useState('');
@@ -45,6 +82,23 @@ export const Courses = () => {
           </Button>
         ))}
       </HStack>
+      <Stack
+        direction={['column', 'row']}
+        justifyContent={['flex-start', 'space-evenly']}
+        alignItems={['center', 'flex-start']}
+        flexWrap="wrap"
+      >
+        <Course
+          view={23}
+          title={'sample'}
+          imgSrc={'sample'}
+          id={'sample'}
+          // addToPlaylistHandler: any;
+          creator={'sample'}
+          description={'sample'}
+          lectureCount={2}
+        />
+      </Stack>
     </Container>
   );
 };
