d9c0ea2a59f63837d0e602efaa8b93f77d378749
Vishal k <vkulkarni149@gmail.com>
2023-03-21 11:24:48 +0530

    added the heading and  input tag

--- src/component/Courses/Courses.jsx
+++ src/component/Courses/Courses.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { Container, Heading, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 export const Courses = () => {
-  return <div>Courses</div>;
+  const [keyword, setKeyword] = useState('');
+  return (
+    <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
+      <Heading children="All COURSES" margin={'8'} />
+      <Input
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        type="text"
+        placeholder={'Search the Course...'}
+        focusBorderColor="yellow.500"
+      />
+    </Container>
+  );
 };
