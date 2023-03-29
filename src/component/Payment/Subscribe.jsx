2ed73076ef3875ad1126586d138472d1941bfb08
Vishal k <vkulkarni149@gmail.com>
2023-03-29 11:42:00 +0530

    created the subscibe page

--- src/component/Payment/Subscribe.jsx
+++ src/component/Payment/Subscribe.jsx
@@ -1,4 +1,11 @@
-import { Container, Heading, VStack, Box, Text } from '@chakra-ui/react';
+import {
+  Container,
+  Heading,
+  VStack,
+  Box,
+  Text,
+  Button,
+} from '@chakra-ui/react';
 import React from 'react';
 
 const Subscribe = () => {
@@ -14,6 +21,32 @@ const Subscribe = () => {
       >
         <Box bg={'yellow.400'} p={'4'} css={{ borderRadius: '8px 8px 0 0  ' }}>
           <Text children={`pro pack - ₹299.00 `} />
+        </Box>
+        <Box p={'4'}>
+          <VStack textAlign={'center'} px="8" spacing={'8'} mt={'4'}>
+            <Text children="Join pro pack get access to all content" />
+            <Heading size={'md'} children="₹299.00 only " />
+          </VStack>
+          <Button my={'8'} w="full" colorScheme={'yellow'}>
+            Buy Now
+          </Button>
+        </Box>
+        <Box
+          bg={'blackAlpha.600'}
+          p={'4'}
+          css={{ borderRadius: '8px 8px 0 0' }}
+        >
+          <Heading
+            color={'white'}
+            size="sm"
+            textTransform={'uppercase'}
+            children={'100% refund at cancellation'}
+          />
+          <Text
+            fontSize={'xs'}
+            color={'white'}
+            children={'Terms & condtion apply'}
+          />
         </Box>
       </VStack>
     </Container>
