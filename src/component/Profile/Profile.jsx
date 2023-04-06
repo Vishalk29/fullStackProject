de5e5efa63ee05962f0357d84503c022817b079a
Vishal k <vkulkarni149@gmail.com>
2023-04-06 18:51:25 +0530

    Here we have created the profile detail and active and subscription logic

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -3,11 +3,25 @@ import {
   Avatar,
   Button,
   Container,
+  HStack,
   Heading,
+  Link,
   Stack,
+  Text,
   VStack,
 } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
 const Profile = () => {
+  const user = {
+    name: 'Vishal',
+    email: 'kulkarni.vishal002@gmail.com',
+    createdAt: String(new Date().toISOString()),
+    role: 'user',
+    subscription: {
+      status: 'active',
+    },
+  };
   return (
     <Container minH={'95vh'} maxW={'container.lg'} py="8">
       <Heading children="Profile" m={'10'} textTransform={'uppercase'} />
@@ -20,10 +34,38 @@ const Profile = () => {
       >
         <VStack>
           <Avatar boxSize={'48'} />
-
           <Button colorScheme={'yellow'} variant={'ghost'}>
             Change Photo
           </Button>
+        </VStack>
+        <VStack spacing={'4'} alignItems={['center', 'flex-start']}>
+          <HStack>
+            <Text children={'Name'} fontWeight={'bold'} />
+            <Text children={user.name} />
+          </HStack>
+          <HStack>
+            <Text children={'Email'} fontWeight={'bold'} />
+            <Text children={user.email} />
+          </HStack>
+          <HStack>
+            <Text children={'createdAt'} fontWeight={'bold'} />
+            <Text children={user.createdAt.split('T')[0]} />
+          </HStack>
+
+          {user.role !== 'admin' && (
+            <HStack>
+              <Text children="Subscription" fontWeight={'bold'} />
+              {user.subscription.status === 'active' ? (
+                <Button colorScheme="yellow" variant={'ghost'}>
+                  Cancel Subscription
+                </Button>
+              ) : (
+                <Link as={ReactRouterLink} to="/subscribe">
+                  <Button colorScheme="yellow">Subscribe</Button>
+                </Link>
+              )}
+            </HStack>
+          )}
         </VStack>
       </Stack>
     </Container>
