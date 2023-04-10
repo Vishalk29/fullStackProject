946118551fdd3212194b63f2f7f39f3d3edcd082
Vishal k <vkulkarni149@gmail.com>
2023-04-10 10:53:51 +0530

    Created the update profile component

--- src/component/Profile/UpdateProfile.jsx
+++ src/component/Profile/UpdateProfile.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 const UpdateProfile = () => {
-  return <div>UpdateProfile</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <Container p={'16'} minH={'90vh'}>
+      <form>
+        <Heading
+          children="Update Profile"
+          my={'16'}
+          textAlign={['center', 'left']}
+          textTransform={'uppercase'}
+        />
+        <VStack spacing={'8'}>
+          <Input
+            required
+            type={'text'}
+            value={name}
+            onChange={e => setName(e.target.value)}
+            placeholder="Name"
+            focusBorderColor="yellow.500"
+          />
+
+          <Input
+            required
+            type={'email'}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="Email"
+            focusBorderColor="yellow.500"
+          />
+          <Button w="full" colorScheme="yellow" type="submit">
+            Change
+          </Button>
+        </VStack>
+      </form>
+    </Container>
+  );
 };
 
 export default UpdateProfile;
