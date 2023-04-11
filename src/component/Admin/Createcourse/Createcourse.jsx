a8973bb6f649f1c6e46b7e1dbf344fdbd4533464
Vishal k <vkulkarni149@gmail.com>
2023-04-11 13:24:39 +0530

    Created the imput tags for title description creater name and select option for category of course

--- src/component/Admin/Createcourse/Createcourse.jsx
+++ src/component/Admin/Createcourse/Createcourse.jsx
@@ -1,4 +1,12 @@
-import { Box, Container, Grid, Heading, Input, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Grid,
+  Heading,
+  Input,
+  Select,
+  VStack,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import cursor from '../../../assets/images/cursor.png';
 import Sidebar from '../Sidebar';
@@ -10,6 +18,13 @@ const Createcourse = () => {
   const [image, setImage] = useState('');
   const [imagePrev, setImagePrev] = useState('');
 
+  const categories = [
+    'web-Development',
+    'Artifical intelligence',
+    'App development',
+    'Game Dvelopment',
+    'Automation Testing',
+  ];
   return (
     <Grid
       minH={'100vh'}
@@ -26,14 +41,42 @@ const Createcourse = () => {
             my={'16'}
             textAlign={['center', 'left']}
           />
-          <Input
-            type={'text'}
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            placeholder="Title"
-            focusBorderColor="purple.300"
-          />
-          <VStack m={'auto'} spacing={'8'}></VStack>
+
+          <VStack m={'auto'} spacing={'8'}>
+            <Input
+              type={'text'}
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Title"
+              focusBorderColor="purple.300"
+            />
+            <Input
+              type={'text'}
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              placeholder="Description"
+              focusBorderColor="purple.300"
+            />
+            <Input
+              type={'text'}
+              value={createdBy}
+              onChange={e => setCreatedBy(e.target.value)}
+              placeholder="Creator Name"
+              focusBorderColor="purple.300"
+            />
+            <Select
+              focusBorderColor="purple.300"
+              value={category}
+              onChange={e => setCategory(e.target.value)}
+            >
+              <option value="">Category</option>
+              {categories.map(item => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+          </VStack>
         </form>
       </Container>
       <Sidebar />
