3f0db1f0ed028f17668f9daa5de599d353aac476
Vishal k <vkulkarni149@gmail.com>
2023-04-12 13:29:47 +0530

    Started to create the Admin course page

--- src/component/Admin/Admincourses/Admincourse.jsx
+++ src/component/Admin/Admincourses/Admincourse.jsx
@@ -1,8 +1,41 @@
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import cursor from '../../../assets/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
 const Admincourse = () => {
+  const users = [
+    {
+      _id: 'sbuabjdbdjn',
+      name: 'Vishal',
+      role: 'admin',
+      subscription: {
+        status: 'active',
+      },
+      email: 'kulkarni.vishal002@gmail.com',
+    },
+  ];
+
+  const updateHandler = userId => {
+    console.log(userId);
+  };
+  const deleteHandler = userId => {
+    console.log(userId);
+  };
   return (
     <Grid
       minH={'100vh'}
@@ -11,9 +44,69 @@ const Admincourse = () => {
         cursor: `url(${cursor}) ,default`,
       }}
     >
-      <Box>HI</Box>
+      <Box padding={['0', '16']} overflow={'auto'}>
+        <Heading
+          textTransform={'uppercase'}
+          children="All Users"
+          my={'16'}
+          textAlign={['center', 'left']}
+        />
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size={'lg'}>
+            <TableCaption>All Available users in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Name</Th>
+                <Th>Email</Th>
+                <Th>Role</Th>
+                <Th>Subscription</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users.map(item => (
+                <Row
+                  updateHandler={updateHandler}
+                  deleteHandler={deleteHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
       <Sidebar />
     </Grid>
   );
 };
+
+function Row({ item, updateHandler, deleteHandler }) {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+      <Td>{item.name}</Td>
+      <Td>{item.email}</Td>
+      <Td>{item.role}</Td>
+      <Td>{item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => updateHandler(item._id)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            Change Role
+          </Button>
+
+          <Button onClick={() => deleteHandler(item._id)} color={'purple.600'}>
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
+
 export default Admincourse;
